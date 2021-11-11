3bae4faa276dd82dba5e5fe8844c4cd1838c4daf
sanjeetaT <sanjeetadthapa@gmail.com>
2021-11-11 14:44:17 -0800

    Basic JavaScript

---
+++ basic_javaScript_project/project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+<script src="main.js"></script>
+window.alert("Hello, world");
+document.write('Hello, world');
+var A = "This is a string";
+document.write(A);
+const string = B;
+window.alert(B);
+document.write("\Be who you are and say what you feel," + "because those who mind don\'t matter and those who matter don\'t mind.\"" +"-Dr.Seuss");
+var B= "concatenated" + " string"
+document.write(B)
+var Family = "The Arezzinis", Dad = "Jermiah", Mom = "Hermoine", Daughter = "Penny" , son = "Zorro";
+document.write(Dad);
+document.write("Lisa told Bart, \"knok it off, Bart! or I'll tell dad!\"<br>\Eat my shorts!\" Bart respponded.");
+
+
+
+var blues = "I have the blues";
+var blue = blues.fontcolor("blue");
+document.write(blues);
+
+  3+ 3
+  document.write(3+3);
+
+  function My_first_Function()// defining a function and naming it
+  {var str = "This text is green!"; // defining a variable and giving it a string value
+  var result = str.fontcolor("green"); // using the fontcolor method on string value
+  document.getElementById("Green_Text").innerHTML = result; // putting the value of result into HTML element
+  }
+ <script src="JS/main.js"></script>
