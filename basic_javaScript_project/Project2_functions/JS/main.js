a2bfbfbad0f2e7996f3fb7ea2ebb9747de9f4a19
sanjeetaT <sanjeetadthapa@gmail.com>
2021-11-11 16:22:41 -0800

    project 2

---
+++ basic_javaScript_project/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+<script src="main.js"></script>
+ function My_first_Function()// defining a function and naming it
+ {var str = "This text is green!"; // defining a variable and giving it a string value
+ var result = str.fontcolor("green"); // using the fontcolor method on string value
+ document.getElementById("Green_Text").innerHTML = result; // putting the value of result into HTML element
+ }
+ var contenDiv = document.getElementById('content')
+ var p = document.querySelectorAll('.newParagraph');
+
+ function myFunction(){    // function
+     var sentence = "I am learning";   // defining variable
+     sentence += "a lot from this book!"; // defining sentence
+     document.getElementById("Concatenate").innerHTML = sentence; // result
+ }
+<script src="JS/main.js"></script>
