feffc7e70936b84e7510dffc8a993b30e8a5ed3d
sanjeetaT <sanjeetadthapa@gmail.com>
2021-11-14 13:04:57 -0800

    project 4

--- basic_javaScript_project/Project4_dictionaries/JS/main.js
+++ basic_javaScript_project/Project4_dictionaries/JS/main.js
@@ -10,15 +10,21 @@ function my_Dictionary(){ // function name
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
 
-function my_Dictionary(){
+function my_Dictionary_one(){
     var Animal = {
         Species:"Dog",
         Color:"Black",
         Breed:"Labrador",
         Age:5,
-        Sound:"Bark!"
+        Sound:"wolf!"
     
     };
     delete Animal.Sound;
-    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+    document.getElementById("Dictionary_one").innerHTML = Animal.Sound;
 }
+
+document.write(typeof "Word");
+
+document.write(typeof 3);
+
+document.write("10" + 5);
